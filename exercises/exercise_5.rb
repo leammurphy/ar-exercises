af9366c23e107232811bb820db5ee0669d758a9c
leam <leam.murphy1@gmail.com>
2021-07-22 02:43:58 +0000

    Can get the total revenue, average revenue, and stores with sales over 1000000

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+puts @total_revenue  # => 4114000
+@average_revenue = Store.average(:annual_revenue)
+puts @average_revenue # => 822800.0
+@big_sales = Store.where('annual_revenue > 1000000').count
+puts @big_sales # => 2
